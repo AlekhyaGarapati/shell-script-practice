852d8a8422c8cc8a05010d39a29c07c71bfc4429
Alekhya <alekhyachowdary@gmail.com>
2023-08-18 01:12:17 +0530

    commit

--- 04-db-variables.sh
+++ 04-db-variables.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "please enter username"
 
-read USERNAME
+read -s USERNAME
 
 echo "username is $USERNAME"
+
+echo "please enter password"
+
+read -s PASSWORD
+
+echo "password is $PASSWORD"
