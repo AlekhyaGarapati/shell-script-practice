8cb71accdfa7125c805f0128f0e479aa6c637d97
Alekhya <alekhyachowdary@gmail.com>
2023-08-18 22:42:14 +0530

    commit

---
+++ 07-functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+VALIDATE()
+{
+ if [ $1 -ne 0 ]
+then
+echo "Installation is incomplete"
+else
+echo "Installation is completed"
+fi
+}
+if [ $USER -ne 0 ]
+then
+echo "you need sudo access to install any software"
+fi
+yum install gitt -y
+VALIDATE $?
+
