4a81b9f76e1d6db9201e23517bca092905223bef
Alekhya <alekhyachowdary@gmail.com>
2023-08-19 01:23:58 +0530

    commit

--- 09-loops.sh
+++ 09-loops.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
+DATE=$(date +%F)
+SCRIPTNAME=$0
+LOGFILE=/tmp/$SCRIPTNAME-$DATE.log
+USER=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0]"
+
+VALIDATE()
+{
+    if ( $1 ne 0)
+    then
+       echo -e "$R $2 Installation not successfull $N"
+    else
+       echo -e "$G $2 Installation not successfull $N"
+}
+if [ $USER -ne 0]
+then
+   echo -e "$R Need Sudo Access to install into Linux $N"
+fi
+
 for i in $@
 do
- yum install $i -y
+ yum install $i -y &>> $LOGFILE
+ VALIDATE $? $i
 done
