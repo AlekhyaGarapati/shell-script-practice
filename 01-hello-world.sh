e9d561a4a5fb62a610ab639fdf135c02d55b3b1a
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-19 19:09:51 +0530

    message

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -6,4 +6,4 @@ person2=$2
 echo "$person1: hi $person2"
 echo "$person2: hi $person1"
 echo "$person1: how r u $person2"
-echo "$person2: Iam good , how r u $person1"
+echo "$person2: Iam good , how r u doing  Alekhya  and $person1"
