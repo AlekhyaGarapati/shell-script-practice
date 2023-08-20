d451e51559a6ef98a794871808b2ef1e85fc152d
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-20 19:05:01 +0530

    commit message

--- 11-delete-old-logs.sh
+++ 11-delete-old-logs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-DELETE_FILES=/home/centos/tmp/apps-logs
+DELETE_FILES_DIR=/home/centos/tmp/apps-logs
 SCRIPTNAME=$0
 DATE=$(date +F%)
 LOG_FILE=$SCRIPTNAME-$DATE.log
 
+DELETE_FILES=$(find $DELETE_FILES_DIR -name "*.log" -type f -mtime +14)
+ecfo "$DELETE_FILES"
 while read line
 do
   echo "Files in delete Folder: $line"
