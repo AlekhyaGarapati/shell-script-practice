dc65bbfb165f78d6b2b023e1fc49bdedc51c2061
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-21 00:39:38 +0530

    commit message

--- 10-palindrome.sh
+++ 10-palindrome.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-WORD=$1
-LENGTH= $(wc -m $WORD)
-echo "Entered Word is $WORD"
-echo "length of the word $LENGTH"
+echo "Please Enter String"
+INPUT_STRING=$1
+REVERSE_STRING=""
+LENGTH=$($INPUT_STRING | wc -m)
+echo "LENGTH : $LENGTH"
+
+
+
 
