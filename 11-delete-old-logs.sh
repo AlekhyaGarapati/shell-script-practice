cf66ef9b940aee6ddb9d7808df5a8ba6bc727409
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-20 19:14:14 +0530

    commit message

--- 11-delete-old-logs.sh
+++ 11-delete-old-logs.sh
@@ -3,11 +3,12 @@
 DELETE_FILES_DIR=/tmp/apps-logs
 SCRIPTNAME=$0
 DATE=$(date +F%)
-LOG_FILE=$SCRIPTNAME-$DATE.log
+LOG_FILE=/tmp/$SCRIPTNAME-$DATE.log
 
 DELETE_FILES=$(find $DELETE_FILES_DIR -name "*.log" -type f -mtime +14)
-echo "$DELETE_FILES"
+echo "$DELETE_FILES 
 while read line
 do
-  echo "Files in delete Folder: $line"
+  echo "Files in delete Folder: $line" &>>$LOG_FILE
+  rm -r $line
 done <<< $DELETE_FILES
