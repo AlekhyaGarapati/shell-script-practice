a6d943253ba025384093560c5a5803e518c28104
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-21 01:29:00 +0530

    commit message

--- 10-palindrome.sh
+++ 10-palindrome.sh
@@ -5,15 +5,16 @@ read input
 INPUT_STRING=$input
 REVERSE_STRING=""
 LENGTH=${#INPUT_STRING}
-echo "LENGTH : $LENGTH"
+
 if [ $LENGTH -ne 0 ]  
 then 
+   
    for (( i=$LENGTH-1; i>=0; i-- ))
    do
      REVERSE_STRING="$REVERSE_STRING${INPUT_STRING:$i:1}"
     done
     if [ $REVERSE_STRING ==  $INPUT_STRING ]
-    then
+        then
         echo "Entered String $INPUT_STRING is Palindrome"
     else
         echo "Entered String $INPUT_STRING is not Palindrome"
