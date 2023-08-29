693f229cefecd8b831b69029e95aa1425e127f8d
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-30 00:10:08 +0530

    rename

--- renaming-files.sh
+++ renaming-files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-RENAME_FILES_DIR= /tmp
-RENAME_FILES= $(find $RENAME_FILES_DIR -name "*.txt" -type f)
+RENAME_FILES_DIR= /home/centos/tmp
+
+RENAME_FILES= $(find $RENAME_FILES_DIR -type f)
 echo $RENAME_FILES
 FILE_NAME=$(basename "$RENAME_FILES")
 
